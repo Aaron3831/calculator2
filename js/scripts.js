2b7765704714781e0daaca8e80c314468c864987
Elan Baharie and Marcel Nogueira <marcel@>
2017-03-20 10:29:38 -0700

    make begining of business logic for calculator

---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+var add = function(number1, number2) {
+  return number1 + number2;
+};
+
+var subtract = function(number1, number2) {
+  return number1 - number2;
+};
+
+var multiply = function(number1, number2) {
+  return number1 * number2;
+};
+
+var divide = function(number1, number2) {
+  return number1 / number2;
+};
+
+var number1 = prompt("Enter first number please");
+
+var number2 = prompt("Enter second number please");
+
+var result = add(number1, number2);
+
+alert(result);
