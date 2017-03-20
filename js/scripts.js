2117a2a669ff0eb720e70980a0cec7d6103276d6
Elan Baharie and Marcel Nogueira <elan@>
2017-03-20 10:47:29 -0700

    finalize business logic

--- js/scripts.js
+++ js/scripts.js
@@ -14,10 +14,10 @@ var divide = function(number1, number2) {
   return number1 / number2;
 };
 
-var number1 = prompt("Enter first number please");
+var number1 = parseInt(prompt("Enter first number please"));
 
-var number2 = prompt("Enter second number please");
+var number2 = parseInt(prompt("Enter second number please"));
 
 var result = add(number1, number2);
 
-alert(result);
+console.log(result);
