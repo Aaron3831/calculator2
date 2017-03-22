960e900046503cb3c824b4cf42297c7c736f0595
Aaron Nguyen and Marcel Nogueira <marcel@>
2017-03-22 15:03:42 -0700

    add subtraction, multiplication and division functionality

--- js/scripts.js
+++ js/scripts.js
@@ -24,5 +24,29 @@ $(document).ready(function() {
   var result = add(number1, number2);
   $("#output").text(result);
   event.preventDefault();
-});
+  });
+
+  $("form#sub").submit(function(event) {
+  var number1 = parseInt($("#sub1").val());
+  var number2 = parseInt($("#sub2").val());
+  var resultSub = subtract(number1, number2);
+  $("#outputsub").text(resultSub);
+  event.preventDefault();
+  });
+
+  $("form#multi").submit(function(event) {
+  var number1 = parseInt($("#multi1").val());
+  var number2 = parseInt($("#multi2").val());
+  var resultMulti = multiply(number1, number2);
+  $("#outputmulti").text(resultMulti);
+  event.preventDefault();
+  });
+
+  $("form#division").submit(function(event) {
+  var number1 = parseInt($("#division1").val());
+  var number2 = parseInt($("#division2").val());
+  var resultDivision = divide(number1, number2);
+  $("#outputdivision").text(resultDivision);
+  event.preventDefault();
+  });
 });
